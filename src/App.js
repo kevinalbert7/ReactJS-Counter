055c705c178dd00f85fae9e1de1b0eeac4b6bfd8
kevinalbert7 <20ko@live.fr>
2021-10-26 14:33:43 +0200

    counter

--- src/App.js
+++ src/App.js
@@ -4,41 +4,43 @@ import React from 'react';
 import { render } from 'react-dom';
 
 
-class App extends React.component {
+class App extends React.Component {
   constructor () {
     super ()
     
     this.state = {
-      count = 0
+      count: 0
     }
   }
   
   handlePlusClick = () => {
-    this.setState({ count: this.state.count: + 1 })
+    this.setState({ count: this.state.count + 1 })
     console.log(this.state)
   }
   
   handleMinusClick = () => {
-    this.setState({ count: this.state.count: - 1 })
+    this.setState({ count: this.state.count - 1 })
     console.log(this.state)
   }
 
   render() {
-    
-    return ()
+      console.log("render")
+    return (
+        <>
+        <h1>Counter</h1>
+        <h2>{this.state.count}</h2>
+        <button onClick={this.handlePlusClick}>+</button>
+        <button onClick={this.handleMinusClick}>-</button>
+        </>
+    )
   }
 }
-
 
 // function App() {
 //   return (
 //     <div className="App">
 //       <header className="App-header">
 //         <img src={logo} className="App-logo" alt="logo" />
-//         <h1>Counter</h1>
-//         <h2>{this.state.count}</h2>
-//         <button onClick={handlePlusClick}>+</button>
-//         <button onClick={handleMinusClick}>-</button>
 //         <p>
 //           Edit <code>src/App.js</code> and save to reload.
 //         </p>
@@ -55,4 +57,6 @@ class App extends React.component {
 //   );
 // }
 
-// export default App;
+
+
+export default App;
