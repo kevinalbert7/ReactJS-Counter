fb992a6f5540c9a98ffc732b0d10c5870b437628
kevinalbert7 <20ko@live.fr>
2022-10-07 19:42:38 +0200

    condition for count1 and count2 are never higher than 100 and never less than 0

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import './App.css';
 import React from 'react';
+import './App.css';
 import FirstCounter from './components/Counter.js'
 import SecondCounter from './components/Counter.js'
 
@@ -12,13 +12,16 @@ class App extends React.Component {
       count2: 0
     }
   }
-
+  
   handlePlusClick1 = () => {
     if (this.state.count1 < 100) {
       if (this.state.count1 < this.state.count2) {
         this.setState({ count1: this.state.count1 + 10 })
       } else {
-        this.setState({ count1: this.state.count1 + 10, count2: this.state.count2 + 10 })
+        this.setState({ 
+          count1: this.state.count1 + 10, 
+          count2: this.state.count2 + 10 
+        })
       }
     }
   }  
@@ -40,7 +43,10 @@ class App extends React.Component {
       if (this.state.count1 < this.state.count2) {
         this.setState({ count2: this.state.count2 - 10 })
       } else {
-        this.setState({ count1: this.state.count1 - 10, count2: this.state.count2 - 10 })
+        this.setState({ 
+          count1: this.state.count1 - 10, 
+          count2: this.state.count2 - 10 
+        })
       }
     }
   }
@@ -49,8 +55,16 @@ class App extends React.Component {
     return (
         <content>
           <h1>Counter</h1>
-          <FirstCounter count={this.state.count1} increment={this.handlePlusClick1} substract={this.handleMinusClick1}/>
-          <SecondCounter count={this.state.count2} increment={this.handlePlusClick2} substract={this.handleMinusClick2}/>
+          <FirstCounter 
+            count={this.state.count1} 
+            increment={this.handlePlusClick1} 
+            substract={this.handleMinusClick1}
+          />
+          <SecondCounter 
+            count={this.state.count2} 
+            increment={this.handlePlusClick2} 
+            substract={this.handleMinusClick2}
+          />
         </content>
     )
   }
